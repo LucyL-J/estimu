a4a1fdbfba82c53107997c65a75386f355e8ecd2
Lucy Lansch-Justen <l.lansch-justen@sms.ed.ac.uk>
2024-06-27 07:58:37 +0100

    pdf and cdf with subpopulation of on-cells

--- MutantCountDistributions.jl
+++ MutantCountDistributions.jl
@@ -4,6 +4,8 @@ using SpecialFunctions
 factorials(K::Int) = Float64.([factorial(big(k)) for k in 0:K])
 reduced_fs(K::Int) = 1 ./ [k*(k+1) for k in 1:K]
 gammas(K::Int, inv_fit_m) = [gamma(1+inv_fit_m + k) for k in 1:K] ./ gamma(1+inv_fit_m) ./ inv_fit_m
+scale_f(f_on, rel_div_on) = (1-f_on)/(1-f_on*(1-rel_div_on))
+inverse_fit_on(f_on, rel_div_on) = (1-f_on*(1-rel_div_on))/rel_div_on
 
 # Mutant count distributions (probabilities to obverse 0:K mutant colonies)
 
@@ -59,4 +61,63 @@ function cdf_mudi(k::Int, N, mu, fit_m=1.)
         p = mudi(mu*N, k, q)
     end
     return sum(p)
+end
+
+# With a subpopulation of on-cells
+function pdf_mudi(k::Int, N, mu_off, mu_on, f_on, rel_div_on, fit_m=1.)
+    if rel_div_on == 0.
+        # on-cells have zero division rate
+        f = factorials(k)
+        p_on = mudi_0(mu_on*N*f_on/(1-f_on), k, f)
+        if fit_m == 1.
+            rf = reduced_fs(k)
+            p_off = mudi(mu_off*N, k, rf)
+        else
+            g = gammas(k, 1/fit_m)
+            p_off = mudi(mu_off*N, k, f[1:end-1]./g)
+        end
+    else
+        f = factorials(k-1)
+        # Re-scale the effective population size (because on-cells now also contribute to growth)
+        N *= scale_f(f_on, rel_div_on)
+        # Calculate differential fitness of on-cells (compared to total population growth)
+        ifit = inverse_fit_on(f_on, rel_div_on)/fit_m
+        g_on = gammas(k, ifit)
+        p_on = mudi(mu_on*N*f_on/(1-f_on), k, f./g_on)
+        if fit_m == 1.
+            rf = reduced_fs(k)
+            p_off = mudi(mu_off*N, k, rf)
+        else
+            g_off = gammas(k, 1/fit_m)
+            p_off = mudi(mu_off*N, k, f./g_off)
+        end
+    end
+    return sum(p_off .* reverse(p_on))    # Total mutant count = sum of contributions from on-cells and the rest of the population
+end
+function cdf_mudi(k::Int, N, mu_off, mu_on, f_on, rel_div_on, fit_m=1.)
+    if rel_div_on == 0.
+        f = factorials(k)
+        p_on = mudi_0(mu_on*N*f_on/(1-f_on), k, f)
+        if fit_m == 1.
+            rf = reduced_fs(k)
+            p_off = mudi(mu_off*N, k, rf)
+        else
+            g = gammas(k, 1/fit_m)
+            p_off = mudi(mu_off*N, k, f[1:end-1]./g)
+        end
+    else
+        f = factorials(k-1)
+        N *= scale_f(f_on, rel_div_on)
+        ifit = inverse_fit_on(f_on, rel_div_on)/fit_m
+        g_on = gammas(k, ifit)
+        p_on = mudi(mu_on*N*f_on/(1-f_on), k, f./g_on)
+        if fit_m == 1.
+            rf = reduced_fs(k)
+            p_off = mudi(mu_off*N, k, rf)
+        else
+            g_off = gammas(k, 1/fit_m)
+            p_off = mudi(mu_off*N, k, f./g_off)
+        end
+    end
+    return sum([sum(p_off[1:i] .* reverse(p_on[1:i])) for i = 1:k+1])
 end
