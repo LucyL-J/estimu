753d4a0990d7ef849c0c557183cf1b49f166435a
LucyL-J <65664690+LucyL-J@users.noreply.github.com>
2024-07-23 16:37:45 +0100

    Classifying antibiotic by target instead of mode of action

--- experimental_data/generate_plots.R
+++ experimental_data/generate_plots.R
@@ -1,5 +1,6 @@
 library("plyr")
 library("ggplot2")
+library("viridisLite")
 library("ggpubr")
 
 # Read data frames
@@ -9,45 +10,25 @@ sum_data <- read.csv("experimental_data/sum_data.csv")[,-1]
 est_paras <- read.csv("experimental_data/est_paras.csv")[,-1]
 est_sum <- read.csv("experimental_data/est_sum.csv")[,-1]
 
-# Group mode of action into 'Wall', 'Pro', 'DNA' and 'Other', pool replicates, and order data frames
-meta_data$mode_of_action <- mapvalues(meta_data$antibiotic, from = antibiotic_classes$antibiotic_abbr, to = antibiotic_classes$my_group)
+# Pool replicates, and order data frames
+meta_data$target_group <- mapvalues(meta_data$antibiotic, from = antibiotic_classes$antibiotic_abbr, to = antibiotic_classes$target_group)
 meta_data <- subset(meta_data, replicate == 0)
 df <- merge(meta_data, est_sum, by = "ID")
-df <- arrange(df, mode_of_action, antibiotic)
-df$antibiotic <- factor(df$antibiotic, levels = unique(df$antibiotic), ordered = TRUE)
+df$antibiotic <- factor(df$antibiotic, levels = antibiotic_classes$antibiotic_abbr, ordered = TRUE)
 df$ID <- factor(df$ID, levels = unique(df$ID), ordered = TRUE)
-df$mode_of_action <- factor(df$mode_of_action, levels = unique(df$mode_of_action), ordered = TRUE)
-antibiotic_classes <- arrange(antibiotic_classes, my_group, antibiotic_abbr)
-antibiotic_classes$antibiotic_abbr <- factor(antibiotic_classes$antibiotic_abbr, levels = unique(antibiotic_classes$antibiotic_abbr), ordered = TRUE)
+df$target_group <- factor(df$target_group, levels = unique(antibiotic_classes$target_group), ordered = TRUE)
+antibiotic_classes$antibiotic_abbr <- factor(antibiotic_classes$antibiotic_abbr, levels = antibiotic_classes$antibiotic_abbr, ordered = TRUE)
+antibiotic_classes$target_group <- factor(antibiotic_classes$target_group, levels = unique(antibiotic_classes$target_group), ordered = TRUE)
 
-# Color-coding antibiotics used in the studies by mode of action 
-u <- unique(antibiotic_classes$my_group)
-antibiotic_classes$color <- ""
-for (g in unique(antibiotic_classes$my_group)){
-  a <- antibiotic_classes$antibiotic[antibiotic_classes$my_group == g]
-  for (j in 1:length(a)){
-    if (g == "DNA"){
-      antibiotic_classes$color[antibiotic_classes$antibiotic == a[j]] <- colorRampPalette(c("#00FFFF", "#000112"))(length(a))[j]
-    } else {
-      if (g == "Wall"){
-        antibiotic_classes$color[antibiotic_classes$antibiotic == a[j]] <- colorRampPalette(c("#008631", "#abf7b1"))(length(a))[j]
-      } else {
-        if (g == "Pro"){
-          antibiotic_classes$color[antibiotic_classes$antibiotic == a[j]] <- colorRampPalette(c("#FFFF9E", "#b30000"))(length(a))[j]
-        } else {
-          antibiotic_classes$color[antibiotic_classes$antibiotic == a[j]] <- colorRampPalette(c("#FF00CC", "#CC00CC"))(length(a))[j]
-        }
-      }
-    }
-  }
-}
-v <- numeric(length(antibiotic_classes$antibiotic))
+# Color-coding antibiotics used in the studies (by target group)
+antibiotic_classes$color <- turbo(length(antibiotic_classes$antibiotic_abbr))
+v <- numeric(length(antibiotic_classes$antibiotic_abbr))
 for (i in 1:length(v)) {
   v[i] <- length(subset(meta_data, antibiotic == antibiotic_classes$antibiotic_abbr[i])$antibiotic)
 }
 antibiotic_classes$prevalence <- v
-p_antibiotic <- ggplot(data = antibiotic_classes, aes(x=my_group, y=prevalence, fill=antibiotic_abbr)) + 
-  geom_bar(stat = "identity") + scale_fill_manual(values = antibiotic_classes$color) + xlab("Mode of action")
+p_antibiotic <- ggplot(data = antibiotic_classes, aes(x=target_group, y=prevalence, fill=antibiotic_abbr)) + 
+  geom_bar(stat = "identity") + scale_fill_manual(values = antibiotic_classes$color) + xlab("Grouped by target") + ylab("Number of experiments")
 p_antibiotic
 
 # Estimated increase in population-wide mutation rate by antibiotic
@@ -73,33 +54,26 @@ p_M_antibiotic <- ggplot(data = df_SIM, aes(x=ID, y=M.1, group=antibiotic)) +
 p_M_antibiotic
 
 # Testing for normality -> not normal
-shapiro.test(subset(df, mode_of_action == "DNA")$M_wo_fitm.1)
+shapiro.test(subset(df, is.element(target_group, c("DNA", "Gyrase")))$M_wo_fitm.1)
 
-# Kruskal-Wallis test and pairwise comparison -> DNA-Pro and DNA-Wall are significantly different
-kruskal.test(df$M_wo_fitm.1, df$mode_of_action)
-pairwise.wilcox.test(df$M_wo_fitm.1, df$mode_of_action)
-compare_means(M_wo_fitm.1 ~ mode_of_action, data = df)
-my_comparison <- list(c("DNA","Other"),c("DNA","Pro"),c("DNA","Wall"))
-p_M_mode <- ggplot(data = df, aes(x=mode_of_action, y=M_wo_fitm.1, fill=mode_of_action)) + geom_boxplot() +
+# Kruskal-Wallis test and pairwise comparison -> DNA and ribosome targeting significantly different
+kruskal.test(df$M_wo_fitm.1, df$target_group)
+print(compare_means(M_wo_fitm.1 ~ target_group, data = df), n = 45)
+# Comparing DNA/gyrase targeting with others
+df$KW <- df$target_group
+levels(df$KW) <- c(levels(df$KW), "DNA_direct_target", "Others")
+df$KW[is.element(df$KW, c("DNA", "Gyrase"))] <- "DNA_direct_target"
+df$KW[df$KW != "DNA_direct_target"] <- "Others"
+wilcox.test(M_wo_fitm.1 ~ KW, data = df)
+p_M_DNA <- ggplot(data = df, aes(x=KW, y=M_wo_fitm.1, fill=KW)) + geom_boxplot() +
   coord_trans(y = "log10", ylim = c(5*10^-2,5*10^2)) + scale_y_continuous(breaks = c(0.1,1,10,100), labels = c(0.1,1,10,100)) +
-  scale_fill_manual(values = c("#009092", "#FF00CC", "#FF6600", "#5ced73")) + 
-  ylab("Estimated fold change in population-wide mutation rate") + 
-  stat_compare_means(comparisons = my_comparison, label.x = 1, label.y = c(70,110,170)) + stat_compare_means(label.y = 400) 
-p_M_mode
-# Comparing DNA - not DNA
-levels(df$mode_of_action) <- c("DNA", "Other", "Pro", "Wall", "not DNA")
-df$mode_of_action[which(is.element(df$mode_of_action, c("Pro","Other","Wall")))] <- "not DNA"
-wilcox.test(M_wo_fitm.1 ~ mode_of_action, data = df)
-p_M_DNA <- ggplot(data = df, aes(x=mode_of_action, y=M_wo_fitm.1, fill=mode_of_action)) + geom_boxplot() +
-  coord_trans(y = "log10", ylim = c(5*10^-2,5*10^2)) + scale_y_continuous(breaks = c(0.1,1,10,100), labels = c(0.1,1,10,100)) +
-  scale_fill_manual(values = c("#009092", "#FF00CC", "#FF6600", "#5ced73")) + 
   ylab("Estimated fold change in population-wide mutation rate") +
   stat_compare_means(label.y = 400) 
 p_M_DNA
 
 # Model selection for experiments with significant increase in population-wide mutation rate
 selected_models <- data.frame(antibiotic=rep(unique(df_SIM$antibiotic), each=3))
-selected_models$mode_of_action <- mapvalues(selected_models$antibiotic, from=antibiotic_classes$antibiotic_abbr, to=antibiotic_classes$my_group) 
+selected_models$target_group <- mapvalues(selected_models$antibiotic, from = antibiotic_classes$antibiotic_abbr, to = as.character(antibiotic_classes$target_group))
 selected_models$m <- rep(c("hom","none","het"), length(unique(df_SIM$antibiotic)))
 criterion <- "by_AIC"
 n <- match(criterion, names(df_SIM))
@@ -111,9 +85,15 @@ for (i in 1:length(unique(df_SIM$antibiotic))) {
 }
 selected_models$prevalence <- v
 
-p_model_selection <- ggplot(data = selected_models, aes(x=factor(m, c("hom","none","het")), y=prevalence, fill=mode_of_action)) + geom_bar(stat = "identity")+ 
-  scale_fill_manual(values = c("#009092", "#FF00CC", "#FF6600", "#5ced73")) + xlab("Selected model") + ggtitle(criterion)
-p_model_selection
+p_msel_a <- ggplot(data = selected_models, aes(x=factor(m, c("hom","none","het")), y=prevalence, fill=antibiotic)) + geom_bar(stat = "identity") + 
+  scale_fill_manual(values = subset(antibiotic_classes, is.element(antibiotic_abbr, unique(df_SIM$antibiotic)))$color) +
+  xlab("Selected model") + ggtitle(criterion) + ylab("Number of experiments")
+p_msel_a
+
+p_msel_t <- ggplot(data = selected_models, aes(x=factor(m, c("hom","none","het")), y=prevalence, fill=target_group)) + geom_bar(stat = "identity") + 
+  scale_fill_manual(values = turbo(length(unique(df_SIM$target_group)))) +
+  xlab("Selected model") + ggtitle(criterion) + ylab("Number of experiments") 
+p_msel_t
 
 # Difference in AIC between homogeneous and heterogeneous-response model
 p_Delta_AIC <- ggplot(data = df_SIM, aes(x=ID, y=Delta_AIC, group=antibiotic)) + geom_point(aes(color=antibiotic)) +
