6ee3a2433f63dddb7f1225dc731b99b00f06bf76
Lucy L-J <lucy.lanju@googlemail.com>
2025-05-20 15:58:20 +0100

    Printing both output dataframes, the one with inferred parameters and with the model selection outcomes

--- test_script.jl
+++ test_script.jl
@@ -8,7 +8,7 @@ Nf_S = 1.6*10^8;
 for eff_UT in [1, 0.9, 0.1]
     for fit_m in (1., 0.8, false)
         println(eff_UT)
-        println(estimu(mc_UT, Nf_UT/eff_UT, eff_UT, fit_m)[1])
+        println(estimu(mc_UT, Nf_UT/eff_UT, eff_UT, fit_m))
     end
 end
 
@@ -16,20 +16,20 @@ for eff_UT in [1, 0.9]
     for eff_S in [1, 0.1]
         for fit_m in [[1.,1.], [0.8,0.4]]
             println((eff_UT, eff_S))
-            println(estimu_0(mc_UT, Nf_UT/eff_UT, mc_S, Nf_S/eff_S, [eff_UT,eff_S], fit_m)[1])
+            println(estimu_0(mc_UT, Nf_UT/eff_UT, mc_S, Nf_S/eff_S, [eff_UT,eff_S], fit_m))
             for f_on in (0.05, false)
                 for rel_div_on in (0., 0.1, false)
                     println((eff_UT, eff_S))
-                    println(estimu_het(mc_UT, Nf_UT/eff_UT, mc_S, Nf_S/eff_S, [eff_UT,eff_S], f_on, rel_div_on, fit_m)[1])
+                    println(estimu_het(mc_UT, Nf_UT/eff_UT, mc_S, Nf_S/eff_S, [eff_UT,eff_S], f_on, rel_div_on, fit_m))
                 end
             end
         end
         for fit_m in ([1.,1.], [0.8,0.4], false)
             println((eff_UT, eff_S))
-            println(estimu_0(mc_UT, Nf_UT/eff_UT, mc_S, Nf_S/eff_S, [eff_UT,eff_S], fit_m)[1])
-            println(estimu_hom(mc_UT, Nf_UT/eff_UT, mc_S, Nf_S/eff_S, [eff_UT,eff_S], fit_m)[1])
+            println(estimu_0(mc_UT, Nf_UT/eff_UT, mc_S, Nf_S/eff_S, [eff_UT,eff_S], fit_m))
+            println(estimu_hom(mc_UT, Nf_UT/eff_UT, mc_S, Nf_S/eff_S, [eff_UT,eff_S], fit_m))
         end
         println((eff_UT, eff_S))
-        println(estimu_hom(mc_UT, Nf_UT/eff_UT, mc_S, Nf_S/eff_S, [eff_UT,eff_S], (false,false))[1])
+        println(estimu_hom(mc_UT, Nf_UT/eff_UT, mc_S, Nf_S/eff_S, [eff_UT,eff_S], (false,false)))
     end
 end
