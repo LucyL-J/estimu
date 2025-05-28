68bbcf146bce3760d11bbd3cca45ce0bb2956ffe
Lucy L-J <lucy.lanju@googlemail.com>
2025-05-28 17:04:52 +0100

    Using log-likelihood as a measure for goodness of fit. The function LL_dist returns a distribution of log-likelihoods of simulated (i.e. randomly drawn) mutant count distributions; for (i) a homogeneous and (ii) a heterogeneous population.

--- goodnessoffit.jl
+++ goodnessoffit.jl
@@ -1,25 +1,45 @@
 using StatsBase
 
-function Hellinger_dist(R::Int, num_c::Int, mc_rdraws, p_rdraws)
-    H = Vector{Float64}(undef, R)
+function LL_dist(R::Int, num_c::Int, N, mu, fit_m, eff)
+    mc_rdraws, p_rdraws = r_mudi(R*num_c, N, mu, fit_m, eff)
+    LL = Vector{Float64}(undef, R)
     s = randperm(R * num_c) 
     c_indices = reshape(s, num_c, R)
-    for i in eachindex(H)
-        mc_observed = mc_rdraws[c_indices[:, i]]
-        p_observed = counts(mc_observed, 0:maximum(mc_observed)) ./ num_c
-        p = p_rdraws[1:maximum(mc_observed)+1]
-        norm_factor = sqrt(sum(p_observed) * sum(p))
-        H[i] = sqrt(1 - sum(sqrt.(p_observed .* p)) / norm_factor)
+    for i in eachindex(LL)
+        mc_obs = mc_rdraws[c_indices[:, i]]
+        mc_counts_obs = counts(mc_obs, 0:maximum(mc_obs))
+        p = p_rdraws[1:maximum(mc_obs)+1]
+        if any(x -> x == 0, p)
+            println("Warning: Zero probability in p_rdraws at index $i")
+            println("p: $p")
+        end
+        LL[i] = -sum(mc_counts_obs .* log.(p)) 
+        if isnan(LL[i])
+            println("Warning: NaN in LL calculation at index $i")
+            LL[i] = Inf  # Assign a large value to avoid NaN
+        end
     end
-    return H
+    return LL
 end
 
-function gof(mc_counts, p_counts, R::Int, num_c, mc_rdraws, p_rdraws)
-    H = Hellinger_dist(R, num_c, mc_rdraws, p_rdraws)
-    p_observed = mc_counts ./ num_c
-    norm_factor = sqrt(sum(p_observed) * sum(p_counts))
-    H_observed = sqrt(1 - sum(sqrt.(p_observed .* p_counts)) / norm_factor)
-    return H_observed, ecdf(H)(H_observed)
+function LL_dist(R::Int, num_c::Int, N, mu_off, S, f_on, rel_div_on, fit_m, eff)
+    mc_rdraws, p_rdraws = r_mudi(R*num_c, N, mu_off, S, f_on, rel_div_on, fit_m, eff)
+    LL = Vector{Float64}(undef, R)
+    s = randperm(R * num_c) 
+    c_indices = reshape(s, num_c, R)
+    for i in eachindex(LL)
+        mc_obs = mc_rdraws[c_indices[:, i]]
+        mc_counts_obs = counts(mc_obs, 0:maximum(mc_obs))
+        p = p_rdraws[1:maximum(mc_obs)+1]
+        if any(x -> x == 0, p)
+            println("Warning: Zero probability in p_rdraws at index $i")
+            println("p: $p")
+        end
+        LL[i] = -sum(mc_counts_obs .* log.(p)) 
+        if isnan(LL[i])
+            println("Warning: NaN in LL calculation at index $i")
+            LL[i] = Inf  # Assign a large value to avoid NaN
+        end
+    end
+    return LL
 end
-
-    
